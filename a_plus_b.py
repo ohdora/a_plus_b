689a6e3099dacc6427c80cf60a741fbc2aa93b83
ohdora <67595376+ohdora@users.noreply.github.com>
2020-09-12 11:00:30 +0500

    Add files via upload

---
+++ a_plus_b.py
@@ -0,0 +1,8 @@
+# Nevzorova D.A Ft-290007
+
+print("To calculate sum of a and b enter only numbers!\n"
+      "All other symbols will break the program!\n")
+a = int(input("Enter the a: a = "))
+b = int(input("Enter the b: b = "))
+s = a + b
+print(f"Sum of a and b = {a} + {b} = {s} ")
